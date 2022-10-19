7210621788379b08a92b6be84131b779ee3860d1
will763 <killzasper@gmail.com>
2022-10-18 21:44:43 -0300

    removes unused environment variables

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -3,26 +3,15 @@ import * as auth from 'firebase/auth'
 import { getAuth } from "firebase/auth";
 import { collection, Firestore, getFirestore } from "firebase/firestore";
 
-import {
-    API_KEY,
-    AUTH_DOMAIN,
-    DATABASE_URL,
-    PROJECT_ID,
-    STORAGE_BUCKET,
-    MESSAGING_SENDER_ID,
-    APP_ID,
-    MEASUREMENT_ID,
-} from 'react-native-dotenv'
-
 const firebaseConfig = {
-    apiKey: API_KEY,
-    authDomain: AUTH_DOMAIN,
-    databaseURL: DATABASE_URL,
-    projectId: PROJECT_ID,
-    storageBucket: STORAGE_BUCKET,
-    messagingSenderId: MESSAGING_SENDER_ID,
-    appId: APP_ID,
-    measurementId: MEASUREMENT_ID
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
 };
 
 const app = firebase.initializeApp(firebaseConfig);
