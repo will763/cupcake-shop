bfe09873df52ca938c3754b86e214022b9a4a68e
will763 <killzasper@gmail.com>
2022-10-27 13:38:07 -0300

    delete service file

--- src/screens/Login/services.ts
+++
@@ -1,7 +0,0 @@
-import { authApp, auth } from "config/firebase.config";
-import { ILogin } from "./model";
-
-
-export const loginWithEmailAndPassword = ({ email, password }: ILogin) => {
-    return auth.signInWithEmailAndPassword(authApp, email, password);
-}
