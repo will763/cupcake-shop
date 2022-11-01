63e3ca626de64576c66eacdcfc62419c9b74bfb7
will763 <killzasper@gmail.com>
2022-11-01 13:39:32 -0300

    add function loginGoogleCredential

--- src/screens/Login/viewModels.ts
+++ src/screens/Login/viewModels.ts
@@ -2,12 +2,18 @@ import { auth, authApp } from "config/firebase.config";
 import { Login } from "./model";
 
 const useLoginViewModel = () => {
-    async function newLogin(email: string, password: string): Promise<Login> {
+
+    async function newLogin(email: string, password: string): Login {
         return auth.signInWithEmailAndPassword(authApp, email, password);
     }
 
+    async function loginGoogleCredential(credential: auth.OAuthCredential): Login {
+        return auth.signInWithCredential(authApp, credential)
+    }
+
     return {
-        newLogin
+        newLogin,
+        loginGoogleCredential
     }
 }
 
