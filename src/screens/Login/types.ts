109000352086db7afc880e51677abe8b31d85f89
will763 <killzasper@gmail.com>
2022-11-01 13:36:45 -0300

    add ResponseType

--- src/screens/Login/types.ts
+++ src/screens/Login/types.ts
@@ -1,4 +1,15 @@
+import { AuthError, TokenResponse } from "expo-auth-session";
+
 export type FormValues = {
     email: string;
     password: string;
 };
+
+export type ResponseType = {
+    type: "error" | "success";
+    errorCode: string | null;
+    error?: AuthError | null | undefined;
+    params: Record<string, string>;
+    authentication: TokenResponse | null;
+    url: string;
+}
