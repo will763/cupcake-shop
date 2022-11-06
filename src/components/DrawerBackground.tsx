74f571db28958d2e24dc2d605b6cadf10621089d
will763 <killzasper@gmail.com>
2022-11-06 16:10:52 -0300

    add logout component

--- src/components/DrawerBackground.tsx
+++ src/components/DrawerBackground.tsx
@@ -1,12 +1,19 @@
 import { View } from 'react-native';
+import Logout from './Logout';
 
 export default function() {
   return (
     <View style={{
         backgroundColor:'#ffffff',
         flex:1,
-        elevation:4
+        elevation:4,
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+        paddingRight: 16
         }}
-    />
+
+    >
+      <Logout />
+    </View>
   );
 }
