2e19ef6fdd18d3fc14b0f82f16302e28b58fc8ac
will763 <killzasper@gmail.com>
2022-11-08 23:47:37 -0300

    index has been renamed to View

--- src/screens/Product/View.tsx
+++ src/screens/Product/View.tsx
@@ -1,82 +1,47 @@
-import { useFocusEffect, useRoute } from '@react-navigation/native';
-import { arrayUnion, collection, doc, DocumentData, DocumentSnapshot, getDoc, setDoc, updateDoc } from 'firebase/firestore';
-import React, { useEffect, useState } from 'react';
-import { View, Text, KeyboardAvoidingView, ScrollView } from 'react-native';
-import Comment from '../../components/Comment';
-import { db } from '../../config/firebase.config';
+import React from 'react';
+import { KeyboardAvoidingView, ScrollView } from 'react-native';
+import Comment from 'components/Comment';
 import { ProductProps } from '../../routes/stack/stack.routes';
-import { Button, Container, ContainerComment, ContainerContent, Content, ContentComment, Description, Image, Input, InputComment, InputContainer, Name, SectionName, TextButton, Title } from './styles';
-import uuid from 'react-native-uuid';
-
 import { Rating } from 'react-native-ratings';
 import { Feather } from '@expo/vector-icons'; 
-import ModalBuy from '../../components/ModalBuy';
-import SkeletonLoading from '../../components/SkeletonLoading';
-import ParagraphSkeleton from '../../components/ParagraphSkeleton';
-import CommentsSkeleton from '../../components/CommentsSkeleton';
-import { showMessage } from 'react-native-flash-message';
+import ModalBuy from 'components/ModalBuy';
+import SkeletonLoading from 'components/SkeletonLoading';
+import ParagraphSkeleton from 'components/ParagraphSkeleton';
+import CommentsSkeleton from 'components/CommentsSkeleton';
+import { useProductViewController } from './viewController';
+import { IComments } from './types';
 
-interface IComments {
-  text: string
-}
+import { 
+  Button, 
+  Container, 
+  ContainerComment, 
+  ContainerContent, 
+  Content, 
+  ContentComment, 
+  Description, 
+  Image, 
+  Input, 
+  InputComment, 
+  InputContainer, 
+  Name, 
+  SectionName, 
+  TextButton, 
+  Title 
+} from './styles';
 
 export function Product({route}:ProductProps) {
-  const [data,setData] = useState<DocumentSnapshot<DocumentData>>();
-  const [showModal,setShowModal] = useState(false);
-  const [isLoading,setIsLoading] = useState(true);
-  const [comments,setComments] = useState<IComments[]>([]);
-  const [comment,setComment] = useState('');
-  const [shouldUpdate,setShouldUpdate] = useState(uuid.v4());
-  const {params} = route;
 
-  function cancelModal(){
-    setShowModal(false);
-  }
-
-  function g(){
-    showMessage({
-       message: 'Thanks for rating',
-       type: "success",
-       titleStyle:{fontSize:18},
-       duration:2000
-      });
-  }
-
-  async function sendComment(){
-    const commentRef = doc(db, 'product', params?.id as string);
-    await updateDoc(commentRef, {
-      comments: arrayUnion({
-        id: uuid.v4() as string,
-        text: comment,
-      })
-    }).then(()=>{
-      setComment('');
-      setShouldUpdate(uuid.v4())
-    })
-
-  }
-
-  useFocusEffect(
-    React.useCallback(() => {
-     const getData = async () => {
-     const myDoc = doc(db, 'product', params?.id as string);
-     const response = await getDoc(myDoc);
-     setData(response);
-     
-     const comments = response.data()?.comments.reverse();
-     setComments(comments);
-     }
-     getData();    
-    }, [shouldUpdate])
-  );
-
-  useEffect(()=>{
-    if(data?.exists()){
-     setIsLoading(false);
-    }
-  }, [data])
-
-
+  const { 
+    cancelModal, 
+    sendComment, 
+    showModal, 
+    isLoading, 
+    data, 
+    setShowModal, 
+    comment, 
+    setComment, 
+    isSending,
+    comments } = useProductViewController(route.params.id);
 
   return (
    <Container>
@@ -95,12 +60,12 @@ export function Product({route}:ProductProps) {
        :
       <Rating
         style={{marginTop:12, marginBottom:25}}
+        readonly={true}
         type='custom'
         tintColor='#F84747'
         fractions={0}
         ratingCount={5}
         imageSize={35}
-        onFinishRating={g}
       /> }
       {isLoading ? <SkeletonLoading height={28} width={200} maxWidth={250} />
        : <Title>Description</Title> }
@@ -141,7 +106,7 @@ export function Product({route}:ProductProps) {
      </ScrollView>
      {isLoading ? null 
       : 
-      <InputComment >
+      <InputComment>
        <InputContainer>
         <Input 
          placeholder='Write a comment...' 
@@ -155,7 +120,7 @@ export function Product({route}:ProductProps) {
          color="black" 
          style={{opacity: comment ? 1 :  .3}} 
          onPress={sendComment}
-         disabled={comment ? false : true}
+         disabled={isSending || (!comment) ? true : false}
         />
        </InputContainer>
       </InputComment>
