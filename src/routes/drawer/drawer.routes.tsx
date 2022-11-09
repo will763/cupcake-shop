678f6b09f30ef33bc88560de138bd1b93abdefa5
will763 <killzasper@gmail.com>
2022-11-08 22:33:23 -0300

    admin screen only appears for admin

--- src/routes/drawer/drawer.routes.tsx
+++ src/routes/drawer/drawer.routes.tsx
@@ -4,6 +4,7 @@ import { CustomizeOrder } from 'screens/CustomizeOrder/View';
 import { Doubts } from 'screens/Doubts/View';
 import { Home } from 'screens/Home/View';
 import { Admin } from 'screens/Admin'
+import { RegisterProduct } from 'screens/RegisterProduct/View'
 
 import { FontAwesome } from '@expo/vector-icons'; 
 
@@ -14,10 +15,24 @@ import IconCupcake from 'components/IconCupcake';
 import IconAdmin from 'components/IconAdmin'
 import DrawerBackground from 'components/DrawerBackground';
 import Logout from 'components/Logout';
+import { useEffect, useState } from 'react';
+import { authApp } from 'config/firebase.config';
 
 const {Navigator, Screen } = createDrawerNavigator();
 
 export function DrawerRoutes() {
+
+  const [ isAdmin, setIsAdmin] = useState(false);
+
+  useEffect(()=> {
+    const unsubscriber = authApp.onAuthStateChanged(user => {
+      user?.email === process.env.ADMIN_TOKEN ? setIsAdmin(true) : setIsAdmin(false)
+    })
+
+    return unsubscriber;
+  }, []);
+
+
   return (
     <Navigator initialRouteName='Home' screenOptions={{
       headerTitleAlign: 'center',
@@ -55,13 +70,18 @@ export function DrawerRoutes() {
        }}
       />
 
-      <Screen
-       name="Admin" 
-       component={Admin} 
-       options={{
-        drawerIcon: () => <IconAdmin /> ,
-       }}
-      />
+      { isAdmin 
+        ? 
+         <Screen
+          name="Admin" 
+          component={RegisterProduct} 
+          options={{
+            drawerIcon: () => <IconAdmin /> ,
+          }}
+         />
+        : null
+      }
+
     </Navigator>
   );
 }
